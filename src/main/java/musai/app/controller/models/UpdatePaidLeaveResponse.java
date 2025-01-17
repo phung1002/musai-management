0e75db0fcc4cb12e88f820e84723a9ad40af6630
Tran Kim Hoang <Alisakimhoang@gmail.com>
2025-01-17 13:52:12 +0900

    Create UpdatePaidLeaveResponse.java

---
+++ src/main/java/musai/app/controller/models/UpdatePaidLeaveResponse.java
@@ -0,0 +1,13 @@
+package musai.app.controller.models;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class UpdatePaidLeaveResponse {
+	
+	private String message;
+
+}
