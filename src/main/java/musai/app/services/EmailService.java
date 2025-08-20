70d4cffa3417098589f6cb60f0b23713138b804a
Chamith9409 <chamith19dj@gmail.com>
2025-08-20 11:02:57 +0900

    add email service

---
+++ src/main/java/musai/app/services/EmailService.java
@@ -0,0 +1,33 @@
+package musai.app.services;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class EmailService {
+
+    private final JavaMailSender mailSender;
+
+    @Value("${app.admin.email}")
+    private String adminEmail;
+
+    @Value("${app.mail.from}")
+    private String fromEmail;
+
+    @Value("${app.mail.from.name}")
+    private String fromName;
+
+    public void sendAdminNotification(String subject, String text) {
+    	System.out.println("gggggggggg");
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(adminEmail);
+        message.setFrom(String.format("%s <%s>", fromName, fromEmail));
+        message.setSubject(subject);
+        message.setText(text);
+        mailSender.send(message);
+    }
+}
